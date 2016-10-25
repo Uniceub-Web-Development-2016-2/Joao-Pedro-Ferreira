2fc1c87192b05ad9548b6d65d36dcf07fe680551
joaoordep <joaopedrozhk@gmail.com>
2016-10-25 19:09:47 -0200

    INSERT, UPDATE e DELETE
    Crud Completo

--- aula6/resource_controller.php
+++ aula6/resource_controller.php
@@ -10,11 +10,24 @@ class ResourceController
 	
 	}
 	private function search($request) {
-		$query = 'SELECT * FROM bd_combustivel.'.$request->getResource().' WHERE '.self::queryParams($request->getParameters());
+		
+		if(empty($request->getParameters())){
+				$query = 'SELECT * FROM bd_combustivel.'.$request->getResource();
+
+
 		
 		$result = (new DBConnector())->query($query);
 		
 		return $result->fetchAll(PDO::FETCH_ASSOC);
+		}else{
+			$query = 'SELECT * FROM bd_combustivel.'.$request->getResource().' WHERE '.self::queryParams($request->getParameters());
+		
+		$result = (new DBConnector())->query($query);
+		
+		return $result->fetchAll(PDO::FETCH_ASSOC);	
+		}
+		
+		
 	}
 		
 	private function queryParams($params) {
@@ -27,6 +40,72 @@ class ResourceController
 		
 		return $query;
 	}
-	
+
+	private function create($request){
+//EXEMPLO REQUEST USUARIO
+//    http://localhost/aula6/tb_usuario?nme_usuario=NETO VIADO&cpf_usuario=123456&psw_usuario=0987&cod_perfil=1&email_usuario=email@neto.com
+
+		if($request->getParameters()){
+			
+			$parametroQuery = "";
+			$parametroQueryColum ="";
+			foreach($request->getParameters() as $key => $value) {
+			$parametroQuery .= '"'.$value.'"'.' , ';	
+			$parametroQueryColum .= ''.$key.''.' , ';
+			}
+
+			
+		$parametroQuery = substr($parametroQuery,0,-3);
+		$parametroQueryColum = substr($parametroQueryColum,0,-3);
+
+		$query = 'INSERT INTO bd_combustivel.'.$request->getResource().' ('.$parametroQueryColum.')VALUES ('.$parametroQuery.')';
+			
+			$result = (new DBConnector())->query($query);
+
+			return $result;
+		}
+	}
+
+	private function update ($request){
+//EXEMPLO REQUEST USUARIO
+//    http://localhost/aula6/tb_usuario?nme_usuario=NETO VIADO&cpf_usuario=123456&psw_usuario=0987&cod_perfil=1&email_usuario=email@neto.com&idt_usuario=1
+
+
+		if($request->getParameters()){
+			
+			$parametroQuery = "";
+			$ultimo ="";
+			foreach($request->getParameters() as $key => $value) {
+			$parametroQuery .= $key.' = '.'"'.$value.'"'.' , ';	
+			$ultimo= $key.'='.$value;
+		
+			}
+
+			
+			
+		 $parametroQuery = substr($parametroQuery,0,-3);
+		$query = 'UPDATE bd_combustivel.'.$request->getResource().' SET '.$parametroQuery.' WHERE '.$ultimo;
+		 	$result = (new DBConnector())->query($query);
+
+		 	return $result;
+		}
+	}	
+
+	private function remove($request){
+
+		//EXEMPLO REQUEST USUARIO
+//    http://localhost/aula6/tb_usuario?idt_usuario=1
+			$parametro = "";
+			if($request->getParameters()){
+			foreach($request->getParameters() as $key => $value) {
+			$parametro.= $key.' = '.'"'.$value.'"'.' , ';	
+			}
+
+			$parametro = substr($parametro,0,-3);
+				$query = 'DELETE FROM bd_combustivel.'.$request->getResource().' WHERE '.$parametro;
+				$result = (new DBConnector())->query($query);
+				return $result;
+			}
+	}
 }
 
